810cdc454135c913a2af9ad74e88d78dc4867d6d
Flavia-Duran <duranpepeu@gmail.com>
2022-10-07 14:47:20 -0300

    codando o último microsserviço

---
+++ ratings-data-service/src/main/java/io/goahead/ratingsdataservice/models/RatingsResource.java
@@ -0,0 +1,19 @@
+package io.goahead.ratingsdataservice.models;
+
+import io.goahead.ratingsdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+ @RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+}
+
+
+
